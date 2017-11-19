e8a2a6daafc4613fc43d4d8b54288c237bfc3f9c
Yifan Ding <yifand1@andrew.cmu.edu>
2017-11-18 23:03:50 -0500

    first commit for path_db package

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/yifan/mygit/path_db/catkin_ws/src
+# Build directory: /home/yifan/mygit/path_db/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(path_db)
---
+++ catkin_ws/build/path_db/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yifan/mygit/path_db/catkin_ws/src/path_db
+# Build directory: /home/yifan/mygit/path_db/catkin_ws/build/path_db
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
