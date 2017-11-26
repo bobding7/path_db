6ae00c1a62317dbcb74bcb2d0e1672f9f20356b3
Yifan Ding <yifand1@andrw.cmu.edu>
2017-11-26 14:17:29 -0500

    remove duplicate folder

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yifan/mygit/path_db/catkin_ws/src
-# Build directory: /home/yifan/mygit/path_db/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(path_db)
